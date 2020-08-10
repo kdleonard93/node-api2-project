c1b5acad6cb92635d9c7d8aafbdb48c39ee13dfe
Kyle Leonard <kdleonard@icloud.com>
2020-08-09 19:24:20 -0500

    Made a posts router and server.

---
+++ posts/posts-router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const posts = require("../data/db");
+
+
+const router = express.Router();
+
+router.get("/api/posts", (req, res) => {
+    posts.find(posts)
+    .then((posts) => {
+        res.status(200).json({message: "Posts found"})
+    })
+    .catch((error) =>{
+        console.log(error);
+        res.status(500).json({message: "Error Retrieving Posts"})
+    })
+})
+
+
+module.exports = router;
+
+
