31796e4ca68f70c0b8c234c467f6eddd67e11e98
Kyle Leonard <kdleonard@icloud.com>
2020-08-11 23:10:54 -0500

    made post req

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -19,13 +19,28 @@ router.get("/api/posts/:id", (req, res) => {
     posts.findById(req.params.id)
     .then((posts) => {
         if(posts) {
-            res.status(200).json(posts)
+            res.status(200).json(posts[0])
         } else {
             res.status(404).json({message: "Could not retrieve Posts"})
         }
     })
     .catch((error) => {
         res.status(500).json({message: "The post information could not be retrieved."})
+    })
+})
+
+router.post("/api/posts", (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        return res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+    }
+    posts.insert(req.body)
+    .then(({id}) => {
+        posts.findById(id).then((post) =>{
+        res.status(201).json(post[0])
+      })
+    })
+    .catch((error) => {
+        res.status(500).json({error: "There was an error while saving the post to the database."})
     })
 })
 
