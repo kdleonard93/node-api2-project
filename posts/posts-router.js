bd6244d9ac96649d6ce51d9e77567c6544fe9996
Kyle Leonard <kdleonard@icloud.com>
2020-10-01 20:00:10 -0500

    Added catch to GET by ID

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -15,19 +15,26 @@ router.get("/api/posts", (req, res) => {
 });
 
 router.get("/api/posts/:id", (req, res) => {
-  posts.findById(req.params.id).then((posts) => {
-    if (post) {
-      res.status(200).json(posts[0]);
-    } else {
-      res.status(404).json({
-        message: "Posts not found",
-      });
-    }
-  });
+  posts
+    .findById(req.params.id)
+    .then((posts) => {
+      if (post) {
+        res.status(200).json(posts[0]);
+      } else {
+        res.status(404).json({
+          message: "Posts not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved." });
+    });
 });
 
-router.post("/api/posts", (req, res) => {
-  posts;
-});
+// router.post("/api/posts", (req, res) => {
+//   posts;
+// });
 
 module.exports = router;
