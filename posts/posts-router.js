d518ab7b0137a85fbe5fb31fd07442dee57d8ba7
Kyle Leonard <kdleonard@icloud.com>
2020-10-05 18:56:14 -0500

    Finished Post-router

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,9 +33,65 @@ router.get("/api/posts/:id", (req, res) => {
     });
 });
 
+router.post("/api/posts", (req, res) => {
+  if(!req.body.title || !req.body.contents) {
+      return res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+  }
+  posts.insert(req.body)
+  .then(({id}) => {
+      posts.findById(id).then((post) =>{
+      res.status(201).json(post[0])
+    })
+  })
+  .catch((error) => {
+      res.status(500).json({error: "There was an error while saving the post to the database."})
+  })
+})
 
-// router.post("/api/posts", (req, res) => {
-//   posts;
-// });
+router.put("/api/posts/:id", (req,res) => {
+  if (!req.body.title || !req.body.contents) {
+      return res.status(400).json({
+          errorMessage: "Please provide title and contents for the post." 
+      })
+  }
+
+  posts.update(req.params.id, req.body)
+  .then((post) => {
+      if(post) {
+          res.status(200).json(post)
+      } else {
+          res.status(404).json({
+              message: "The post with the specified ID does not exist." 
+          })
+      }
+  })
+  .catch((error) => {
+      console.log(error)
+      res.status(500).json({
+          error: "The post information could not be modified." 
+      })
+  })
+})
+
+router.delete("/api/posts/:id", (req,res) => {
+posts.remove(req.params.id)
+.then((count) => {
+  if (count > 0) {
+      res.status(200).json({
+          message: "The user has been nuked",
+      })
+  } else {
+      res.status(404).json({
+          message: "The user could not be found",
+      })
+  }
+})
+.catch((error) => {
+  console.log(error)
+  res.status(500).json({
+      message: "Error removing the user",
+  })
+})
+})
 
 module.exports = router;
